1b321a726752536e855b4a21fa0f6b1a6e0b24b8
melcd238 <melcd238@gmail.com>
2023-05-04 13:16:42 +0200

    add principal article in home page

--- src/presentation/components/principalArticle.tsx
+++ src/presentation/components/principalArticle.tsx
@@ -1,38 +1,42 @@
-import imgOrdi from '../assets/imgOrdi.jpg';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../redux/store";
+import { fetchArticles } from "../thunks/fetchArticles";
+
 
 
 const PrincipalArticle = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const principalArticle = useSelector((state: RootState) => state.article.articles[0]);
+  const loading = useSelector((state: RootState) => state.article.loading);
+
+  useEffect(() => {
+    dispatch(fetchArticles());
+  }, [dispatch]);
+   
+  if (loading) {
+    return <div>Chargement...</div>;
+  } 
+
     return (
       <div className="mx-10 my-16">
          <div className="flex-1">
-              <h1 className="text-3xl font-bold">Titre de l'article</h1>
-              <h2 className="text-lg text-gray-400 mb-2 italic">Sous-titre de l'article</h2>
-              <p className="text-gray-400 mb-2 italic">Date de publication - Dernière publication</p>
-              <div className="bg-blue-500 text-white rounded px-2 py-1 inline-block mt-2">Reconversion</div>
+              <h1 className="text-3xl font-bold mb-2">{principalArticle?.title}</h1>
+              <h2 className="text-lg text-gray-400 mb-2 italic">{principalArticle?.summary}</h2>
+              <p className="text-gray-400 mb-2 italic">{principalArticle?.publicationDate} - Dernier article publié</p>
+              <div className="bg-blue-500 text-white rounded px-2 py-1 inline-block mt-2">{principalArticle?.tag}</div>
     
          <div className="w-1/4 mt-4">
-              <img src={imgOrdi} alt="orinateur"  className='rounded'/>
+              <img src={principalArticle?.imgUrl} alt="orinateur"  className='rounded'/>
          </div>
        </div>
 
-        <p className="leading-8 my-5">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel nulla sed metus elementum mattis. Curabitur
-          vel risus at metus condimentum interdum. Duis bibendum neque eu nisl finibus luctus. Sed varius, enim sit amet
-          maximus fermentum, massa leo venenatis nibh, eu posuere quam ipsum a odio. Fusce non sapien non nisi semper
-          consectetur quis non ex. Fusce tempor fringilla mi, ac aliquet ante. Aenean ut dui in enim pellentesque
-          aliquet. Sed cursus ipsum vitae turpis auctor dictum. Sed pharetra, leo sed tincidunt faucibus, arcu tellus
-          auctor urna, sit amet aliquet odio augue quis ante. Integer et tellus eu dolor interdum finibus. Nunc blandit,
-          erat vel molestie blandit, nibh quam iaculis elit, sed volutpat urna odio vel ipsum.
-        </p>
-        <p className="leading-8 my-5">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel nulla sed metus elementum mattis. Curabitur
-          vel risus at metus condimentum interdum. Duis bibendum neque eu nisl finibus luctus. Sed varius, enim sit amet
-          maximus fermentum, massa leo venenatis nibh, eu posuere quam ipsum a odio. Fusce non sapien non nisi semper
-          consectetur quis non ex. Fusce tempor fringilla mi, ac aliquet ante. Aenean ut dui in enim pellentesque
-          aliquet. Sed cursus ipsum vitae turpis auctor dictum. Sed pharetra, leo sed tincidunt faucibus, arcu tellus
-          auctor urna, sit amet aliquet odio augue quis ante. Integer et tellus eu dolor interdum finibus. Nunc blandit,
-          erat vel molestie blandit, nibh quam iaculis elit, sed volutpat urna odio vel ipsum.
-        </p>
+        <div className="my-10" >
+        {principalArticle?.content.split("\n").map((paragraph, index) => (
+           <p key={index} className="leading-8">{paragraph}</p>
+         ))}
+        </div>
+        
       </div>
     
     );
